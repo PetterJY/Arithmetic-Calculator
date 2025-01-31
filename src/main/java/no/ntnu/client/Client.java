7a3ca9a486311cbcfae88b9406ee82db58a47a21
Petter J. Ytterdahl <petterjy@hotmail.no>
2025-01-31 12:08:37 +0100

    Add basic TCP functionality to Client side.

--- src/main/java/no/ntnu/client/Client.java
+++ src/main/java/no/ntnu/client/Client.java
@@ -1,5 +1,41 @@
 package no.ntnu.client;
 
-public class Client {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
+public class Client {
+  public static final int TCP_PORT = 1238;
+
+  private Socket socket;
+
+  private BufferedReader reader;
+  private PrintWriter writer;
+
+  private boolean running;
+
+  public static void main(String[] args) {
+    Client client = new Client();
+    client.establishConnection();
+  }
+
+  public Client() {
+    this.running = false;
+  }
+
+  public void establishConnection() {
+    try {
+      this.socket = new Socket("localhost", TCP_PORT);
+    } catch (IOException e) {
+      System.out.println("Failed to connect to the server: " + e.getMessage());
+    } try {
+      this.reader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+      this.writer = new PrintWriter(this.socket.getOutputStream(), true);
+    } catch (IOException e) {
+      System.out.println("Failed to create Writer and Reader: " + e.getMessage());
+      throw new IllegalArgumentException("Could not create writer and reader");
+    }
+  }
 }
--- src/main/java/no/ntnu/server/Server.java
+++ src/main/java/no/ntnu/server/Server.java
@@ -20,8 +20,7 @@ public class Server {
 		try (ServerSocket serverSocket = new ServerSocket(TCP_PORT)) {
 			this.clientHandler = new ClientHandler(this, serverSocket.accept());
 		} catch (IOException e) {
-			System.out.println("Could not start the server.");
-			e.printStackTrace();
+			System.out.println("Failed to start the server: " + e.getMessage());
 		}
 	}
 }
