90c9ce4943b981b40f2c39a298f608e1bf49834b
Petter J. Ytterdahl <petterjy@hotmail.no>
2025-01-31 11:34:06 +0100

    Add TCP connection to Server and ClientHandler side.

--- src/main/java/no/ntnu/server/ClientHandler.java
+++ src/main/java/no/ntnu/server/ClientHandler.java
@@ -1,5 +1,17 @@
-package no.ntnu;
+package no.ntnu.server;
+
+import java.net.Socket;
 
 public class ClientHandler {
+  private Server server;
+  private Socket socket;
 
+  public ClientHandler(Server server, Socket socket) {
+    this.server = server;
+    this.socket = socket;
+  }
+
+  public void start() {
+    
+  }
 }
--- src/main/java/no/ntnu/server/Server.java
+++ src/main/java/no/ntnu/server/Server.java
@@ -1,5 +1,27 @@
-package no.ntnu;
+package no.ntnu.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
 
 public class Server {
+	public static final int TCP_PORT = 1238; 
+	private ClientHandler clientHandler;
 
+	public static void main(String[] args) {
+		Server server = new Server();
+		server.run();
+	}
+
+	public void run() {
+		this.clientHandler.start();
+	}
+
+	public Server() {
+		try (ServerSocket serverSocket = new ServerSocket(TCP_PORT)) {
+			this.clientHandler = new ClientHandler(this, serverSocket.accept());
+		} catch (IOException e) {
+			System.out.println("Could not start the server.");
+			e.printStackTrace();
+		}
+	}
 }
